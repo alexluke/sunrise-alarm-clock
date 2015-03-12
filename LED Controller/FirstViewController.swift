db2b65987330b1280d6eaef2417dfc3d876a0158
Alex Luke <alex@alexluke.me>
2015-03-11 17:08:55 -0700

    Initial Commit

---
+++ LED Controller/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LED Controller
+//
+//  Created by Alex Luke on 3/11/15.
+//  Copyright (c) 2015 Alex Luke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
