64348ca5b90e20bb079eaef4c40cd670a3d43433
Alex Luke <alex@alexluke.me>
2015-03-13 13:41:14 -0700

    Bluetooth connection

--- LED Controller/AppDelegate.swift
+++ LED Controller/AppDelegate.swift
@@ -7,15 +7,21 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, CBCentralManagerDelegate, CBPeripheralDelegate {
 
     var window: UIWindow?
-
+    var btManager: CBCentralManager?
+    var currentAlert: UIAlertView?
+    var peripheral: CBPeripheral?
+    var rxCharacteristic: CBCharacteristic?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+
+        btManager = CBCentralManager(delegate: self, queue: nil)
+
         return true
     }
 
@@ -35,12 +41,56 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
     }
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func centralManagerDidUpdateState(central: CBCentralManager!) {
+        if (central.state == CBCentralManagerState.PoweredOn) {
+            currentAlert = UIAlertView(title: "Connecting...", message: nil, delegate: self, cancelButtonTitle: nil)
+            currentAlert?.show()
+            
+            NSLog("Starting scan")
+            central.scanForPeripheralsWithServices([CBUUID(string: "6E400001-B5A3-F393-E0A9-E50E24DCCA9E")], options: nil)
+        } else {
+            currentAlert = UIAlertView(title: "Bluetooth Unavailable", message: nil, delegate: self, cancelButtonTitle: nil)
+            currentAlert?.show()
+        }
+    }
 
-
+    func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: [NSObject : AnyObject]!, RSSI: NSNumber!) {
+        NSLog("Discovered peripheral")
+        central.stopScan()
+        
+        self.peripheral = peripheral
+        central.connectPeripheral(self.peripheral, options: nil)
+    }
+    
+    func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
+        NSLog("Connected!")
+        peripheral.delegate = self
+        
+        peripheral.discoverServices([CBUUID(string: "6E400001-B5A3-F393-E0A9-E50E24DCCA9E")])
+        
+    }
+    
+    func centralManager(central: CBCentralManager!, didFailToConnectPeripheral peripheral: CBPeripheral!, error: NSError!) {
+        NSLog("Error connecting: %s", error)
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverServices error: NSError!) {
+        // Only interested in sending data at the moment
+        peripheral.discoverCharacteristics([CBUUID(string: "6E400003-B5A3-F393-E0A9-E50E24DCCA9E")], forService: peripheral.services[0] as CBService)
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverCharacteristicsForService service: CBService!, error: NSError!) {
+        rxCharacteristic =  service.characteristics[0] as? CBCharacteristic
+        
+        // Finally done connecting
+        currentAlert?.dismissWithClickedButtonIndex(0, animated: true)
+    }
 }
 
