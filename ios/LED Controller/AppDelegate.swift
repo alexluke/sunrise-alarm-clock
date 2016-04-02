ed743ce1564612711619f58a5628819427fa18f8
Alex Luke <alex@alexluke.me>
2016-04-02 15:13:17 -0700

    XCode update

--- ios/LED Controller/AppDelegate.swift
+++ ios/LED Controller/AppDelegate.swift
@@ -56,7 +56,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CBCentralManagerDelegate,
             NSLog("Starting scan")
             central.scanForPeripheralsWithServices([CBUUID(string: "6E400001-B5A3-F393-E0A9-E50E24DCCA9E")], options: nil)
         } else {
-            currentAlert = UIAlertView(title: "Bluetooth Unavailable", message: nil, delegate: self, cancelButtonTitle: nil)
+            currentAlert = UIAlertView(title: "Bluetooth Unavailable", message: nil, delegate: self, cancelButtonTitle: "OK")
             currentAlert?.show()
         }
     }
